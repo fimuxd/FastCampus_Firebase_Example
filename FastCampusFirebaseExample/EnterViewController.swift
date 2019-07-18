06a02a261cbedcc18db6d369517291d0e7278c2f
fimuxd <fimuxd@gmail.com>
2019-07-18 23:31:50 +0900

    Modify enter view controller

--- FastCampusFirebaseExample/EnterViewController.swift
+++ FastCampusFirebaseExample/EnterViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2019 Boyoung Park. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Firebase
 import FirebaseDatabase
+import FirebaseAnalytics
 
 class EnterViewController: UIViewController {
     var ref: DatabaseReference!
+    var remoteConfig: RemoteConfig!
     
+    @IBOutlet weak var helloTextLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var birthdayTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
@@ -22,20 +26,20 @@ class EnterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.ref = Database.database().reference()
+        remoteConfigSetting()
         self.attribute()
     }
     
     private func attribute() {
         [nameTextField, birthdayTextField, emailTextField]
             .forEach { $0?.delegate = self }
-        
         self.nameTextField.becomeFirstResponder()
-        
         self.enterButton.isEnabled = false
     }
     
     @IBAction func enterButtonTapped(_ sender: UIButton) {
         self.saveUserInfo()
+        Analytics.logEvent("입력하기_버튼_클릭", parameters: nil)
     }
     
     private func saveUserInfo() {
@@ -57,7 +61,6 @@ class EnterViewController: UIViewController {
             .childByAutoId()
             .setValue(userInfo) { [weak self] error, _ in
                 if let `error` = error {
-                    
                     presentAlert(base: self, type: .submitFailed(error: error))
                 } else {
                     self?.statusLabel.text = "데이터가 성공적으로 저장되었습니다."
@@ -76,5 +79,53 @@ extension EnterViewController: UITextFieldDelegate {
         let nameTextFilled = self.nameTextField.text != ""
         let emailTextFilled = self.emailTextField.text != ""
         self.enterButton.isEnabled = nameTextFilled && emailTextFilled
+    }
+}
+
+extension EnterViewController {
+    enum Configuration: String {
+        case helloTextHidden = "hello_text_hidden"
+        case helloText = "hello_text"
+    }
+    
+    private func remoteConfigSetting() {
+        self.remoteConfig = RemoteConfig.remoteConfig()
+        let settings = RemoteConfigSettings()
+        remoteConfig.configSettings = settings
+        fetchConfig()
+    }
+    
+    private func fetchConfig() {
+        self.helloTextLabel.isHidden = isHelloTextLabelHidden()
+        self.helloTextLabel.text = getHelloText()
+        
+        // 캐시된 데이터를 버리고 다시 불러오는 데 걸리는 시간
+        let expirationDuration: Int = 0
+        remoteConfig
+            .fetch(withExpirationDuration: TimeInterval(expirationDuration)) { [weak self] (status, error) -> Void in
+                if status == .success {
+                    self?.remoteConfig.activateFetched()
+                } else {
+                    print("Config not fetched")
+                    print("Error \(error!.localizedDescription)")
+                }
+        }
+    }
+    
+    private func isHelloTextLabelHidden() -> Bool {
+        guard let isHiddenText = remoteConfig[Configuration.helloTextHidden.rawValue].stringValue,
+            let isHidden = Bool(isHiddenText) else {
+                return true
+        }
+        
+        return isHidden
+    }
+    
+    private func getHelloText() -> String {
+        guard let helloText = remoteConfig[Configuration.helloText.rawValue].stringValue else {
+            return "안녕하세요"
+        }
+        
+        return helloText
     }
 }
