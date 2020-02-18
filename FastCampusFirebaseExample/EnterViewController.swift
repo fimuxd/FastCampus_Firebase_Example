12b6e8fde24764d63e6052dfbc285f911aac628e
박보영 <boyoung@rainist.com>
2020-02-18 10:24:16 +0900

    update FirebaseUI with layout

--- FastCampusFirebaseExample/EnterViewController.swift
+++ FastCampusFirebaseExample/EnterViewController.swift
@@ -11,9 +11,13 @@ import Firebase
 import FirebaseDatabase
 import FirebaseAnalytics
 
+import FirebaseUI
+
 class EnterViewController: UIViewController {
     var ref: DatabaseReference!
     var remoteConfig: RemoteConfig!
+    
+    weak var authUI: FUIAuth?
     
     @IBOutlet weak var helloTextLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
@@ -25,9 +29,42 @@ class EnterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        emailSignInCheck()
+        googleSignInCheck()
         self.ref = Database.database().reference()
+        
         remoteConfigSetting()
         self.attribute()
+    }
+    
+    //MARK: FirebaseUI/EmailAuth
+//    private func emailSignInCheck() {
+//        authUI = FUIAuth.defaultAuthUI()
+//        authUI?.delegate = self
+//        authUI?.providers = [FUIEmailAuth()]
+//        authUI?.shouldHideCancelButton = true
+//
+//        guard let authViewController = authUI?.authViewController() else {
+//            return
+//        }
+//        authViewController.modalPresentationStyle = .fullScreen
+//
+//        self.present(authViewController, animated: true, completion: nil)
+//    }
+    
+    //MARK: FirebaseUI/Google
+    private func googleSignInCheck() {
+        authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+        authUI?.providers = [FUIGoogleAuth(), FUIEmailAuth()]
+        authUI?.shouldHideCancelButton = true
+        
+        guard let authViewController = authUI?.authViewController() else {
+            return
+        }
+        authViewController.modalPresentationStyle = .fullScreen
+        
+        self.present(authViewController, animated: true, completion: nil)
     }
     
     private func attribute() {
@@ -88,9 +125,11 @@ extension EnterViewController {
         case helloText = "hello_text"
     }
     
+    //MARK: RemoteConfig 구성
     private func remoteConfigSetting() {
         self.remoteConfig = RemoteConfig.remoteConfig()
         let settings = RemoteConfigSettings()
+        settings.minimumFetchInterval = 0
         remoteConfig.configSettings = settings
         fetchConfig()
     }
@@ -100,11 +139,11 @@ extension EnterViewController {
         self.helloTextLabel.text = getHelloText()
         
         // 캐시된 데이터를 버리고 다시 불러오는 데 걸리는 시간
-        let expirationDuration: Int = 0
+        let expirationDuration: Int = 0 //초 단위
         remoteConfig
             .fetch(withExpirationDuration: TimeInterval(expirationDuration)) { [weak self] (status, error) -> Void in
                 if status == .success {
-                    self?.remoteConfig.activateFetched()
+                    self?.remoteConfig.activate(completionHandler: nil)
                 } else {
                     print("Config not fetched")
                     print("Error \(error!.localizedDescription)")
@@ -123,9 +162,15 @@ extension EnterViewController {
     
     private func getHelloText() -> String {
         guard let helloText = remoteConfig[Configuration.helloText.rawValue].stringValue else {
-            return "안녕하세요"
+            return "Hi"
         }
         
         return helloText
+    }
+}
+
+extension EnterViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        
     }
 }
